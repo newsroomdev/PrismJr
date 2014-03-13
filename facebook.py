8b202aca785d2f46f92f84d6a34d4c61711dcd6d
geraldarthur <gerald.a.rich@gmail.com>
2014-03-13 09:17:05 -0400

    paginate facebook

--- facebook.py
+++ facebook.py
@@ -4,21 +4,24 @@ from csv import DictReader, DictWriter
 from datetime import datetime
 from os import environ
 from sys import argv
+from time import sleep
 
 from facepy import GraphAPI
 
 # (command, argument, input, output) = argv[1:]
 
 # get posts until that date
-CUTOFF = datetime(2013, 10, 1)
-CUTOFF_UNIX = calendar.timegm(CUTOFF.timetuple())
+start = datetime(2013, 06, 21)
+start_unix = calendar.timegm(start.timetuple())
+stop = datetime(2013, 11, 1)
+stop_unix = calendar.timegm(stop.timetuple())
 
 # get secrets
 OAUTH_TOKEN = environ.get('FB_OAUTH_TOKEN')
 graph = GraphAPI(OAUTH_TOKEN)
 data = ()
 
-with open('faces.csv', 'rb') as read:
+with open('missing_faces.csv', 'rb') as read:
 	data = list(DictReader(read))
 
 # create a list of majority Twitter accounts strings
@@ -56,33 +59,35 @@ def text_cond(possible):
 for face in faces['Majority_Face']:
 	print '%s, %s' % (face, face_count)
 	face_count += 1
-	search = '/posts?since=%s' % CUTOFF_UNIX
-	statuses = graph.get(face + search)
-	if len(statuses['data']) > 1:
-		for status in statuses['data']:
-			timestamp_s = status['created_time']
-			upstamp_s = status['updated_time']
-			timestamp = datetime.strptime(timestamp_s, '%Y-%m-%dT%X+0000')
-			upstamp = datetime.strptime(upstamp_s, '%Y-%m-%dT%X+0000')
-			timeline['data'].append({
-				"id": "%s" % status['from']['id'],
-				"screen_name": "%s" % status['from']['name'],
-				"type": "%s" % status['type'],
-				"created_at": "%s" % timestamp.strftime("%x %X"),
-				"updated_at": "%s" % upstamp.strftime("%x %X"),
-				"week": "%s" % timestamp.strftime("%U"),
-			})
-			text_cond("message")
-			text_cond("story")
-			condition("likes")
-			condition("comments")
-			condition("shares")
-			print '%s\t' % counter
-			counter += 1
+	search = '/posts?since=%s&until=%s' % (start_unix, stop_unix)
+	statuses = graph.get(face + search, page=True, retry=3)
+	for status_info in statuses:
+		if status_info['data']:
+			status_list = status_info['data']
+			for status in status_list:
+				timestamp_s = status['created_time']
+				upstamp_s = status['updated_time']
+				timestamp = datetime.strptime(timestamp_s, '%Y-%m-%dT%X+0000')
+				upstamp = datetime.strptime(upstamp_s, '%Y-%m-%dT%X+0000')
+				timeline['data'].append({
+					"id": "%s" % status['from']['id'],
+					"screen_name": "%s" % status['from']['name'],
+					"type": "%s" % status['type'],
+					"created_at": "%s" % timestamp.strftime("%x %X"),
+					"updated_at": "%s" % upstamp.strftime("%x %X"),
+					"week": "%s" % timestamp.strftime("%U"),
+				})
+				text_cond("message")
+				text_cond("story")
+				condition("likes")
+				condition("comments")
+				condition("shares")
+				print '%s\t' % counter
+				counter += 1
 
 row0 = timeline['data'][0].keys()
 
-with open('facebook.csv', 'wb') as writef:
+with open('missing_maj_facebook.csv', 'wb') as writef:
 	writef.write(u'\ufeff'.encode('utf8'))
 	# Write the header row because of reasons.
 	write_csv = DictWriter(writef, fieldnames=row0)
